5e24a7225ca6cbf25c3118fdba213683881e0d80
skybarer <akashdhar.apssdc@gmail.com>
2022-01-31 00:48:18 +0530

    input css added

--- src/atoms/Input/Input.js
+++ src/atoms/Input/Input.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
+import './Input.css'
 // https://github.com/iiison/react-form
 // https://academind.com/tutorials/reactjs-a-custom-useform-hook
 // https://react-hook-form.com/
@@ -25,45 +25,48 @@ const Input = (
     }
 
     return (
-        <input
-            id={id}
-            type={type}
-            className={className}
-            placeholder={placeholder}
-            onInput={handleInputChange}
-            onChange={onChange}
-            aria-label="Your username or email"
-            aria-invalid="false"
-            value={inputValue}
-            name="username"
-            aria-required="true"
-            aria-describedby="tooltip-input-1"
-            style={{
-                color: 'black',
-                backgroundColor: '#f3f7f7',
-                border: '0',
-                // border: '1px solid #c2c7d0',
-                borderRadius: '0',
-                minWidth: '320px',
-                width: '100%',
-                height: '40px',
-                outline: 'none',
-                fontSize: '18px',
-                padding: '1px 10px',
-                boxShadow: 'inset 0 1px 4px rgb(0 0 0 / 10%), 0 0 0 transparent',
-                marginBottom: '0',
-                boxSizing: 'border-box',
-                fontWeight: '400',
-                style
+        <>
+            {type}
+            <input
+                id={id}
+                type={type}
+                className={className}
+                placeholder={placeholder}
+                onInput={handleInputChange}
+                onChange={onChange}
+                aria-label="Your username or email"
+                aria-invalid="false"
+                value={inputValue}
+                name="username"
+                aria-required="true"
+                aria-describedby="tooltip-input-1"
+                style={{
+                    color: 'black',
+                    backgroundColor: '#f3f7f7',
+                    border: '.1px dashed #c2c7d0',
+                    borderRadius: '0',
+                    minWidth: '320px',
+                    width: '100%',
+                    height: '40px',
+                    outline: 'none',
+                    fontSize: '18px',
+                    padding: '1px 10px',
+                    boxShadow: 'inset 0 1px 4px rgb(0 0 0 / 10%), 0 0 0 transparent',
+                    marginBottom: '0',
+                    boxSizing: 'border-box',
+                    fontWeight: '400',
+                    style
 
-            }}>
-        </input>
+                }}>
+            </input>
+        </>
+
     );
 }
 
 Input.propTypes = {
     id: PropTypes.string,
-    type: PropTypes.string,
+    type: PropTypes.string.isRequired,
     defaultValue: PropTypes.string,
     placeholder: PropTypes.string,
     className: PropTypes.string,
@@ -72,6 +75,7 @@ Input.propTypes = {
 };
 
 Input.defaultProps = {
+    type: 'text',
     onChange: undefined,
 };
 
