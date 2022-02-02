6ec03ac5bcb0bfb70155e2de4f69fa4df5cb99da
skybarer <akashdhar.apssdc@gmail.com>
2022-02-03 00:09:10 +0530

    remoed basic from image story title

--- src/atoms/Images/Images.stories.js
+++ src/atoms/Images/Images.stories.js
@@ -11,7 +11,7 @@ import {
 } from '@storybook/addon-docs';
 
 export default {
-  title: 'atoms/Images/Basic',
+  title: 'atoms/Images',
   component: Images,
   parameters: {
     options: { selectedPanel: 'storybook/a11y/panel' },
