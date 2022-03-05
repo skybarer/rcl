5ac6ebb5f59ea44b3d34faa95bb6061e30075019
skybarer <akashdhar.apssdc@gmail.com>
2022-03-06 02:33:04 +0530

    centered layout and added content

--- src/atoms/Cards/Cards.stories.js
+++ src/atoms/Cards/Cards.stories.js
@@ -66,16 +66,16 @@ const Template = (args) => (
 
 export const Basic = Template.bind({});
 Basic.parameters = {
-
+  layout: 'centered'
 };
 
 Basic.args = {
   className: 'card card',
-  // content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+  content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
   style: {
     padding: "10px 20px",
     overflow: "auto",
-    textAlign: "center",
+    // textAlign: "center",
     border: "thin solid #ccc",
   }
 };
