783df3506587144d4300e29235e33fda10a807af
inkollu <akashdhar@gmail.com>
2023-12-17 15:08:07 +0530

    svg icons added

---
+++ src/assets/icons/UserIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const UserIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="7" r="4" />
+    <path d="M5 22s1.5-3 3-3h8c1.5 0 3 3 3 3" />
+  </svg>
+);
+
+export default UserIcon;
