479ced7856f1582f705ad24e92167084d837a478
skybarer <akashdhar.apssdc@gmail.com>
2024-01-01 12:34:59 +0530

    colors table added

--- src/atoms/Table/index.js
+++ src/atoms/Table/index.js
@@ -1,5 +1,10 @@
 import Table from './Table';
-
+import TableCell from './TableCell';
+import TableHeader from './TableHeader';
+import TableRow from './TableRow';
 export {
-    Table
+    Table,
+    TableCell,
+    TableHeader,
+    TableRow
 };
--- src/electrons/colors/ColorPalette.stories.js
+++ src/electrons/colors/ColorPalette.stories.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ColorPalette from '../colors/colorPalette'; // Import your color palette object
 import styled from 'styled-components';
 import colorJson from './colors.json';
+import Table from '../../atoms/Table/Table';
+import TableHeader from '../../atoms/Table/TableHeader';
+import TableRow from '../../atoms/Table/TableRow';
+import TableCell from '../../atoms/Table/TableCell';
 
 const ColorSwatch = styled.div`
   width: 100px;
@@ -20,71 +24,167 @@ export default {
   },
 };
 
+export const TestColors = () => (
+  <div>
+
+    {/* {Object.keys(colorJson).map((colorItem) => (
+      <Table>
+        <thead>
+          <TableRow>
+            <TableHeader>Index</TableHeader>
+            <TableHeader>Name</TableHeader>
+            <TableHeader>CSS Variable</TableHeader>
+            <TableHeader>HexValue</TableHeader>
+            <TableHeader>Color</TableHeader>
+          </TableRow>
+        </thead>
+        <tbody>
+          {Object.keys(colorItem).map((colorKey, index) => (
+            <TableRow>
+              <TableCell>{index}</TableCell>
+              <TableCell>{colorItem[colorKey]}</TableCell>
+              <TableCell>bs-{colorKey}</TableCell>
+              <TableCell>{colorItem[colorKey]}</TableCell>
+              <TableCell style={{ backgroundColor: colorItem[colorKey] }} ></TableCell>
+            </TableRow>
+          ))}
+        </tbody>
+      </Table>
+
+    ))} */}
+  </div >
+);
+
 export const BaseColors = () => (
   <div>
     <h2>Base Colors</h2>
-    {Object.keys(colorJson.baseColors).map((colorKey) => (
-      <div key={colorKey}>
-        <span>{colorKey}:</span>
-        <span>{colorJson.baseColors[colorKey]}</span>
-        <ColorSwatch key={colorKey} color={colorJson.baseColors[colorKey]} />
-      </div>
-    ))}
-  </div>
+    <Table>
+      <thead>
+        <TableRow>
+          <TableHeader>Index</TableHeader>
+          <TableHeader>Name</TableHeader>
+          <TableHeader>CSS Variable</TableHeader>
+          <TableHeader>HexValue</TableHeader>
+          <TableHeader>Color</TableHeader>
+        </TableRow>
+      </thead>
+      <tbody>
+        {Object.keys(colorJson.baseColors).map((colorKey, index) => (
+          <TableRow>
+            <TableCell>{index}</TableCell>
+            <TableCell>{colorKey}</TableCell>
+            <TableCell>bs-{colorKey}</TableCell>
+            <TableCell>{colorJson.baseColors[colorKey]}</TableCell>
+            <TableCell style={{ backgroundColor: colorJson.baseColors[colorKey] }} ></TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
+  </div >
 );
 
 export const TextColors = () => (
   <div>
     <h2>Text Colors</h2>
-    {Object.keys(colorJson.textColors).map((colorKey) => (
-      <div key={colorKey}>
-        <span>{colorKey}:</span>
-        <span>{colorJson.textColors[colorKey]}</span>
-        <ColorSwatch key={colorKey} color={colorJson.textColors[colorKey]} />
-      </div>
-    ))}
+    <Table>
+      <thead>
+        <TableRow>
+          <TableHeader>Index</TableHeader>
+          <TableHeader>Name</TableHeader>
+          <TableHeader>CSS Variable</TableHeader>
+          <TableHeader>HexValue</TableHeader>
+          <TableHeader>Color</TableHeader>
+        </TableRow>
+      </thead>
+      <tbody>
+        {Object.keys(colorJson.textColors).map((colorKey, index) => (
+          <TableRow>
+            <TableCell>{index}</TableCell>
+            <TableCell>{colorKey}</TableCell>
+            <TableCell>{colorJson.textColors[colorKey]}</TableCell>
+            <TableCell style={{ backgroundColor: colorJson.textColors[colorKey] }} ></TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
 export const BorderColors = () => (
   <div>
     <h2>Border Colors</h2>
-    {Object.keys(colorJson.borderColors).map((colorKey) => (
-      <div key={colorKey}>
-        <span>{colorKey}:</span>
-        <span>{colorJson.borderColors[colorKey]}</span>
-        <ColorSwatch key={colorKey} color={colorJson.borderColors[colorKey]} />
-      </div>
-    ))}
+    <Table>
+      <thead>
+        <TableRow>
+          <TableHeader>Index</TableHeader>
+          <TableHeader>Name</TableHeader>
+          <TableHeader>HexValue</TableHeader>
+          <TableHeader>Color</TableHeader>
+        </TableRow>
+      </thead>
+      <tbody>
+        {Object.keys(colorJson.borderColors).map((colorKey, index) => (
+          <TableRow>
+            <TableCell>{index}</TableCell>
+            <TableCell>{colorKey}</TableCell>
+            <TableCell>{colorJson.borderColors[colorKey]}</TableCell>
+            <TableCell style={{ backgroundColor: colorJson.borderColors[colorKey] }} ></TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
 export const ButtonColors = () => (
   <div>
     <h2>Border Colors</h2>
-    {Object.keys(colorJson.buttonColors).map((colorKey) => (
-      <div key={colorKey}>
-        <span>{colorKey}:</span>
-        <span>{colorJson.buttonColors[colorKey]}</span>
-        <ColorSwatch key={colorKey} color={colorJson.buttonColors[colorKey]} />
-      </div>
-    ))}
+    <Table>
+      <thead>
+        <TableRow>
+          <TableHeader>Index</TableHeader>
+          <TableHeader>Name</TableHeader>
+          <TableHeader>HexValue</TableHeader>
+          <TableHeader>Color</TableHeader>
+        </TableRow>
+      </thead>
+      <tbody>
+        {Object.keys(colorJson.buttonColors).map((colorKey, index) => (
+          <TableRow>
+            <TableCell>{index}</TableCell>
+            <TableCell>{colorKey}</TableCell>
+            <TableCell>{colorJson.buttonColors[colorKey]}</TableCell>
+            <TableCell style={{ backgroundColor: colorJson.buttonColors[colorKey] }} ></TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
 export const AdditionalColors = () => (
   <div>
-    <h2>Border Colors</h2>
-    {Object.keys(colorJson.additionalColors).map((colorKey) => (
-      <div key={colorKey}>
-        <span>{colorKey}:</span>
-        <span>{colorJson.additionalColors[colorKey]}</span>
-        <ColorSwatch
-          key={colorKey}
-          color={colorJson.additionalColors[colorKey]}
-        />
-      </div>
-    ))}
+    <h2> Additional Colors</h2>
+    <Table>
+      <thead>
+        <TableRow>
+          <TableHeader>Index</TableHeader>
+          <TableHeader>Name</TableHeader>
+          <TableHeader>HexValue</TableHeader>
+          <TableHeader>Color</TableHeader>
+        </TableRow>
+      </thead>
+      <tbody>
+        {Object.keys(colorJson.additionalColors).map((colorKey, index) => (
+          <TableRow>
+            <TableCell>{index}</TableCell>
+            <TableCell>{colorKey}</TableCell>
+            <TableCell>{colorJson.additionalColors[colorKey]}</TableCell>
+            <TableCell style={{ backgroundColor: colorJson.additionalColors[colorKey] }} ></TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 // Add more stories for Button Colors, Additional Colors, etc.
