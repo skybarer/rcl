afbb4d622c787fb5268b25d885270ee8a175579f
inkollu <akashdhar@gmail.com>
2024-04-16 21:10:37 +0530

    dropdown component modified

--- src/atoms/Dropdown/Dropdown.js
+++ src/atoms/Dropdown/Dropdown.js
@@ -1,33 +1,44 @@
 // Dropdown.js
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import Divider from './../Divider/Divider';
 const Wrapper = styled.div`
   position: relative;
   display: inline-block;
 `;
 
 const Button = styled.button`
-  background-color: #3498db;
-  color: white;
-  padding: 10px 20px;
+  background-color: white;
+  color: black;
+  padding-top: 0.5rem;
+  padding-bottom: 0.2rem;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
   border: none;
   cursor: pointer;
+  border: 1px solid #bbbbbc;
+  border-radius: 0.375rem;
+  font-weight: bold;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
 `;
 
 const DropdownList = styled.ul`
   position: absolute;
   top: 100%;
-  left: 0;
+  right: 0;
   background-color: #ffffff;
   list-style: none;
   padding: 0;
   margin: 0;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-top: 0.5rem;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+  border-radius: 0.375rem;
+  border: 1px solid #d7d7d8;
+  width: 14rem;
 `;
 
 const DropdownItem = styled.li`
-  padding: 10px;
+  padding: 0.5rem;
   cursor: pointer;
   &:hover {
     background-color: #f1f1f1;
@@ -35,27 +46,51 @@ const DropdownItem = styled.li`
 `;
 
 const Dropdown = ({ options, onSelect }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleItemClick = (option) => {
-        setIsOpen(false);
-        onSelect(option);
-    };
+  const handleItemClick = (option) => {
+    setIsOpen(false);
+    onSelect(option);
+  };
 
-    return (
-        <Wrapper>
-            <Button onClick={() => setIsOpen(!isOpen)}>Select an Option</Button>
-            {isOpen && (
-                <DropdownList>
-                    {options.map((option, index) => (
-                        <DropdownItem key={index} onClick={() => handleItemClick(option)}>
-                            {option}
-                        </DropdownItem>
-                    ))}
-                </DropdownList>
-            )}
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Button onClick={() => setIsOpen(!isOpen)}>
+        Options
+        <span style={{ verticalAlign: 'middle', marginRight: '-0.25rem' }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.25rem"
+            height="1.25rem"
+            viewBox="0 0 20 20"
+            fill="rgb(156 163 175)"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </span>
+      </Button>
+      {isOpen && (
+        <DropdownList>
+          {options.map((option, index) => (
+            <DropdownItem key={index} onClick={() => handleItemClick(option)}>
+              {option}
+            </DropdownItem>
+          ))}
+          <Divider />
+          {options.map((option, index) => (
+            <DropdownItem key={index} onClick={() => handleItemClick(option)}>
+              {option}
+            </DropdownItem>
+          ))}
+        </DropdownList>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Dropdown;
--- src/atoms/Dropdown/Dropdown.stories.js
+++ src/atoms/Dropdown/Dropdown.stories.js
@@ -2,55 +2,53 @@ import React from 'react';
 import Dropdown from './Dropdown';
 
 import {
-    Title,
-    Subtitle,
-    Description,
-    Primary,
-    ArgsTable,
-    Stories,
-    PRIMARY_STORY,
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
 } from '@storybook/addon-docs';
 
 export default {
-    title: 'atoms/Dropdown',
-    component: Dropdown,
-    parameters: {
-        options: { selectedPanel: 'storybook/a11y/panel' },
-        docs: {
-            page: () => (
-                <>
-                    <Title />
-                    <Subtitle />
-                    <Description />
-                    <Primary />
-                    <ArgsTable story={PRIMARY_STORY} />
-                    <Stories />
-                </>
-            ),
-        },
+  title: 'atoms/Dropdown',
+  component: Dropdown,
+  parameters: {
+    options: { selectedPanel: 'storybook/a11y/panel' },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
     },
-    argTypes: {
-        backgroundColor: { control: 'color' },
-
-    }
+  },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 };
 
-const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+const options = ['Account settings', 'Support', 'License', 'Sign out'];
 
 const Template = (args) => (
-    <>
-        <Dropdown options={options} onSelect={() => ('Option Selected')} />
-    </>
+  <>
+    <Dropdown options={options} onSelect={() => 'Option Selected'} />
+  </>
 );
 
 export const Basic = Template.bind({});
 Basic.parameters = {
-    // layout: 'fullscreen'
-
+  layout: 'centered',
 };
 
 Basic.args = {
-    label: 'Dropdowns',
-    // backgroundColor: 'aliceblue',
-    // color: '#000000'
+  label: 'Dropdowns',
+  // backgroundColor: 'aliceblue',
+  // color: '#000000'
 };
