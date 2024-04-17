31b6b50b2c983064e16e5689f455d6cb52cf1a15
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-17 21:16:37 +0530

    card component added

--- src/atoms/Card/Card.js
+++ src/atoms/Card/Card.js
@@ -4,34 +4,37 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const CardWrapper = styled.div`
-  width: min-content;
-
-  /* width: 300px;
-  padding: 20px; */
+  width: ${(props) => props.width || 'auto'};
+  height: ${(props) => props.height || 'auto'};
   resize: both;
-  border-radius: 0.5rem;
   background-color: ${(props) =>
     props.variant === 'filled' ? 'rgb(255 255 255 / 1)' : 'transparent'};
+  border-radius: 0.5rem;
   border: ${(props) =>
     props.variant === 'outlined' ? '1px solid #d7d7d8' : '0 solid #e5e7eb;'};
-  color: ${(props) => (props.variant === 'filled' ? 'black' : '#3b82f6')};
-  padding: 0.375rem;
+  color: ${(props) => (props.variant === 'filled' ? 'black' : 'black')};
+  padding: 0.75rem;
   box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
   box-sizing: border-box;
-  /* border: 0 solid #e5e7eb; */
 `;
 
-const Card = ({ variant, children }) => (
-  <CardWrapper variant={variant}>{children}</CardWrapper>
+const Card = ({ variant, children, width, height }) => (
+  <CardWrapper variant={variant} width={width} height={height}>
+    {children}
+  </CardWrapper>
 );
 
 Card.propTypes = {
   variant: PropTypes.oneOf(['filled', 'outlined']),
   children: PropTypes.node.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 Card.defaultProps = {
   variant: 'filled',
+  width: 'auto',
+  height: 'auto',
 };
 
 export default Card;
--- src/atoms/Card/Card.stories.js
+++ src/atoms/Card/Card.stories.js
@@ -8,9 +8,21 @@ export default {
 };
 
 export const Filled = () => (
-  <Card variant="filled">This is a filled card.</Card>
+  <Card variant="filled" width="15rem">
+    Don’t want to be one of those websites that blinds people when they open it
+    on their phone at 2am? Enable dark mode in your configuration file then
+    throw dark: in front of any color utility to apply it when dark mode is
+    active. Works for background colors, text colors, border colors, and even
+    gradients.
+  </Card>
 );
 
 export const Outlined = () => (
-  <Card variant="outlined">This is an outlined card.</Card>
+  <Card variant="outlined" width="15rem">
+    Don’t want to be one of those websites that blinds people when they open it
+    on their phone at 2am? Enable dark mode in your configuration file then
+    throw dark: in front of any color utility to apply it when dark mode is
+    active. Works for background colors, text colors, border colors, and even
+    gradients.
+  </Card>
 );
