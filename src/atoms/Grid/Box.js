7fc6f084bcfba3305bc1a3c593c6bcbc043b0f90
inkollu <akashdhar@gmail.com>
2024-04-17 16:46:12 +0530

    grid component added

--- src/atoms/Grid/Box.js
+++ src/atoms/Grid/Box.js
@@ -1,10 +1,20 @@
 // Box.js
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Box = styled.div`
   border: 1px solid #ddd;
   padding: 20px;
   text-align: center;
+  grid-column-end: span ${(props) => props.colSpan};
 `;
+
+Box.propTypes = {
+  colSpan: PropTypes.number,
+};
+
+Box.defaultProps = {
+  colSpan: 1,
+};
 
 export default Box;
--- src/atoms/Grid/Grid.stories.js
+++ src/atoms/Grid/Grid.stories.js
@@ -5,12 +5,17 @@ import Grid from './Grid';
 import Box from './Box';
 
 storiesOf('Grid', module).add('default', () => (
-  <Grid columns={3} gap={10}>
+  <Grid columns={3} gap={20}>
     <Box>Box 1</Box>
     <Box>Box 2</Box>
     <Box>Box 3</Box>
     <Box>Box 4</Box>
     <Box>Box 5</Box>
     <Box>Box 6</Box>
+    <Box>Box 1</Box>
+    <Box>Box 2</Box>
+    <Box>Box 3</Box>
+    <Box colSpan={2}>This box spans 2 columns</Box>
+    <Box colSpan={1}>Box 3</Box>
   </Grid>
 ));
