d00edaf6d121f14f25b57b8acf1f7e27bac398cf
inkollu <akashdhar@gmail.com>
2024-04-17 19:59:57 +0530

    added secondary color pallete

--- src/atoms/ColorPicker/ColorPicker.stories.js
+++ src/atoms/ColorPicker/ColorPicker.stories.js
@@ -11,18 +11,35 @@ export default {
 };
 
 export const Default = () => (
-  <Card variant="outlined">
-    <Grid>
-      <ColorPicker
-        colors={['#eff6ff', '#dbeafe', '#bfdbfe', '#93c5fd', '#60a5fa']}
-        onColorSelect={action('color selected')}
-      />
-    </Grid>
-    <Grid variant="outlined">
-      <ColorPicker
-        colors={['#60a5fa', '#3b82f6', '#2563eb', '#1d4ed8', '#1e40af']}
-        onColorSelect={action('color selected')}
-      />
-    </Grid>
-  </Card>
+  <>
+    <Card variant="outlined">
+      <Grid>
+        <ColorPicker
+          colors={['#eff6ff', '#dbeafe', '#bfdbfe', '#93c5fd', '#60a5fa']}
+          onColorSelect={action('color selected')}
+        />
+      </Grid>
+      <Grid variant="outlined">
+        <ColorPicker
+          colors={['#60a5fa', '#3b82f6', '#2563eb', '#1d4ed8', '#1e40af']}
+          onColorSelect={action('color selected')}
+        />
+      </Grid>
+    </Card>
+
+    <Card variant="outlined">
+      <Grid>
+        <ColorPicker
+          colors={['#f8fafc', '#f1f5f9', '#e2e8f0', '#cbd5e1', '#94a3b8']}
+          onColorSelect={action('color selected')}
+        />
+      </Grid>
+      <Grid variant="outlined">
+        <ColorPicker
+          colors={['#64748b', '#475569', '#334155', '#1e293b', '#0f172a']}
+          onColorSelect={action('color selected')}
+        />
+      </Grid>
+    </Card>
+  </>
 );
