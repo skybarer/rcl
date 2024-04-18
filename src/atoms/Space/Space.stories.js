8231a300a0ed1aefb51dbdf517a7bc6d80f3e509
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-18 09:17:36 +0530

    added space component

---
+++ src/atoms/Space/Space.stories.js
@@ -0,0 +1,26 @@
+// Card.stories.js
+import React from 'react';
+import Space from './Space';
+
+export default {
+  title: 'Space',
+  component: Space,
+};
+
+const customScale = {
+  1: '10px',
+  2: '20px',
+  // ... add the rest of your scale here
+};
+
+export const Default = () => (
+  <>
+    <Space size={1}>
+      <p>Your content here</p>
+    </Space>
+
+    <Space size={1} customScale={customScale}>
+      <p>Your content here</p>
+    </Space>
+  </>
+);
