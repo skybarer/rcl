67aabed37ba4a44ffcad99e41ff5c73117403dcd
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-18 09:17:59 +0530

    changed the card varient

--- src/atoms/ColorPicker/ColorPicker.stories.js
+++ src/atoms/ColorPicker/ColorPicker.stories.js
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions';
 import ColorPicker from './ColorPicker';
 import Card from '../Card/Card';
 import Grid from '../Grid/Grid';
+import Space from './../Space/Space';
 
 export default {
   title: 'ColorPicker',
@@ -12,29 +13,29 @@ export default {
 
 export const Default = () => (
   <>
-    <Card variant="outlined">
+    <Card variant="filled" width="max-content">
       <Grid>
         <ColorPicker
           colors={['#eff6ff', '#dbeafe', '#bfdbfe', '#93c5fd', '#60a5fa']}
           onColorSelect={action('color selected')}
         />
       </Grid>
-      <Grid variant="outlined">
+      <Grid variant="filled">
         <ColorPicker
           colors={['#60a5fa', '#3b82f6', '#2563eb', '#1d4ed8', '#1e40af']}
           onColorSelect={action('color selected')}
         />
-      </Grid>
+      </Grid> 
     </Card>
-
-    <Card variant="outlined">
+    <Space size={5}></Space>
+    <Card variant="filled" width="max-content">
       <Grid>
         <ColorPicker
           colors={['#f8fafc', '#f1f5f9', '#e2e8f0', '#cbd5e1', '#94a3b8']}
           onColorSelect={action('color selected')}
         />
       </Grid>
-      <Grid variant="outlined">
+      <Grid variant="filled">
         <ColorPicker
           colors={['#64748b', '#475569', '#334155', '#1e293b', '#0f172a']}
           onColorSelect={action('color selected')}
