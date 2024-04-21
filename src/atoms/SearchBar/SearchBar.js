e359f4f007aef16f08592653218d77e4e66ba527
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-21 12:48:20 +0530

    modified serach bar css

--- src/atoms/SearchBar/SearchBar.js
+++ src/atoms/SearchBar/SearchBar.js
@@ -3,14 +3,31 @@ import styled from 'styled-components';
 
 const SearchInput = styled.input`
   padding: 10px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  width: 300px;
+  appearance: none;
+  border: 1px solid #cacaca;
+  border-radius: 6px;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: black;
+  height: 50px;
+  letter-spacing: 0;
+  line-height: 20px;
+  line-height: 18px;
+  outline: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 35rem;
+
+  &:hover {
+    border: 1px solid #b180d1;
+  }
 `;
 
 const SearchBar = ({ placeholder, onChange }) => {
-    return <SearchInput type="text" placeholder={placeholder} onChange={onChange} />;
+  return (
+    <SearchInput type="text" placeholder={placeholder} onChange={onChange} />
+  );
 };
 
 export default SearchBar;
