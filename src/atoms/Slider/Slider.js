c351865ebc6a6127b5baf1883e842b405d90e409
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-21 14:07:02 +0530

    slider component added

--- src/atoms/Slider/Slider.js
+++ src/atoms/Slider/Slider.js
@@ -5,17 +5,41 @@ import styled from 'styled-components';
 const SliderWrapper = styled.input.attrs({ type: 'range' })`
   width: 100%;
   margin: 10px 0;
+  height: 4px !important;
+  -webkit-tap-highlight-color: transparent;
+  appearance: none;
+  background: #f1f1f1;
+  border-radius: 3px;
+  cursor: pointer;
+  height: 5px;
+  outline: 0;
+  position: relative;
+  width: 100%;
+  background: ${(props) =>
+    `linear-gradient(90deg, rgb(99, 0, 163) ${props.value}%, rgb(247, 244, 248) ${props.value}%)`};
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: ${(props) => `rgb(99, 0, 163, 1)`};
+    border: 1px solid white;
+    cursor: pointer;
+  }
 `;
 
 const Slider = ({ value, onChange, min, max }) => {
-    return <SliderWrapper value={value} onChange={onChange} min={min} max={max} />;
+  return (
+    <SliderWrapper value={value} onChange={onChange} min={min} max={max} />
+  );
 };
 
 Slider.propTypes = {
-    value: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
-    min: PropTypes.number,
-    max: PropTypes.number,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number,
 };
 
 export default Slider;
--- src/atoms/Slider/Slider.stories.js
+++ src/atoms/Slider/Slider.stories.js
@@ -1,61 +1,60 @@
 import React from 'react';
 import Slider from './Slider';
 import {
-    Title,
-    Subtitle,
-    Description,
-    Primary,
-    ArgsTable,
-    Stories,
-    PRIMARY_STORY,
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
 } from '@storybook/addon-docs';
 
 export default {
-    title: 'atoms/Slider',
-    component: Slider,
-    parameters: {
-        options: { selectedPanel: 'storybook/a11y/panel' },
-        docs: {
-            page: () => (
-                <>
-                    <Title />
-                    <Subtitle />
-                    <Description />
-                    <Primary />
-                    <ArgsTable story={PRIMARY_STORY} />
-                    <Stories />
-                </>
-            ),
-        },
+  title: 'atoms/Slider',
+  component: Slider,
+  parameters: {
+    options: { selectedPanel: 'storybook/a11y/panel' },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
     },
-    argTypes: {
-        backgroundColor: { control: 'color' },
-
-    }
+  },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 };
 
 const Template = (args) => {
-    const [value, setValue] = React.useState(50);
+  const [value, setValue] = React.useState(50);
 
-    const handleSliderChange = (event) => {
-        setValue(parseInt(event.target.value, 10));
-    };
+  const handleSliderChange = (event) => {
+    setValue(parseInt(event.target.value, 10));
+  };
 
-    return (
-        <>
-            <Slider value={value} onChange={handleSliderChange} min={0} max={100} /> {value}
-        </>
-    );
+  return (
+    <>
+      <Slider value={value} onChange={handleSliderChange} min={0} max={100} />
+      {value}
+    </>
+  );
 };
 
 export const Circular = Template.bind({});
 Circular.parameters = {
-    layout: 'centered'
-
+  layout: 'centered',
 };
 
 Circular.args = {
-    label: 'Spinners',
-    backgroundColor: 'aliceblue',
-    color: '#000000'
+  label: 'Spinners',
+  backgroundColor: 'aliceblue',
+  color: '#000000',
 };
