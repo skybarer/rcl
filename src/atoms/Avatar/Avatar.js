5b4b154ba879d9cd8d7cd0ee7a01488533fad97f
Inkollu <akashdhar.apssdc@gmail.com>
2024-04-24 21:19:21 +0530

    modifed styles of avatar component

--- src/atoms/Avatar/Avatar.js
+++ src/atoms/Avatar/Avatar.js
@@ -1,25 +1,53 @@
-// Avatar.js
+// src/components/Avatar.js
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledAvatar = styled.img`
-  width: ${(props) => props.size}px;
-  height: ${(props) => props.size}px;
-  border-radius: 50%;
+const getBorderRadius = (variant) => {
+  switch (variant) {
+    case 'circle':
+      return '50%';
+    case 'square':
+      return '0';
+    case 'rounded-square':
+      return '10%';
+    default:
+      return '50%'; // Default to circle if variant is not specified
+  }
+};
+
+const AvatarWrapper = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  border-radius: ${({ variant }) => getBorderRadius(variant)};
 `;
 
-export const Avatar = ({ src, size, alt }) => (
-  <StyledAvatar src={src} size={size} alt={alt} />
-);
+const AvatarImage = styled.img`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  object-fit: cover;
+`;
+
+const Avatar = ({ src, alt, size, variant, defaultImage }) => {
+  return (
+    <AvatarWrapper variant={variant}>
+      <AvatarImage src={src || defaultImage} alt={alt} size={size} />
+    </AvatarWrapper>
+  );
+};
 
 Avatar.propTypes = {
-  src: PropTypes.string.isRequired,
-  size: PropTypes.number,
-  alt: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  variant: PropTypes.oneOf(['circle', 'square', 'rounded-square']),
+  defaultImage: PropTypes.string,
 };
 
 Avatar.defaultProps = {
-  size: 50,
-  alt: 'Avatar',
+  size: '50px',
+  variant: 'circle',
+  defaultImage: 'https://via.placeholder.com/50',
 };
+
+export default Avatar;
--- src/atoms/Avatar/Avatar.stories.js
+++ src/atoms/Avatar/Avatar.stories.js
@@ -1,60 +1,42 @@
+// src/stories/Avatar.stories.js
 import React from 'react';
-import { Avatar } from './Avatar';
-import {
-  Title,
-  Subtitle,
-  Description,
-  Primary,
-  ArgsTable,
-  Stories,
-  PRIMARY_STORY,
-} from '@storybook/addon-docs';
+import Avatar from './Avatar';
 
 export default {
-  title: 'atoms/Avatar',
+  title: 'Atoms/Avatar',
   component: Avatar,
-  parameters: {
-    options: { selectedPanel: 'storybook/a11y/panel' },
-    docs: {
-      page: () => (
-        <>
-          <Title />
-          <Subtitle />
-          <Description />
-          <Primary />
-          <ArgsTable story={PRIMARY_STORY} />
-          <Stories />
-        </>
-      ),
-    },
-  },
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
-const Template = (args) => {
-  return (
-    <>
-      <Avatar
-        src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-        size={50}
-        alt="Small Avatar"
-      />
-      <Avatar
-        src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-        size={100}
-        alt="Large Avatar"
-      />
-    </>
-  );
+const Template = (args) => <Avatar {...args} />;
+
+export const Circle = Template.bind({});
+Circle.args = {
+  src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+  alt: 'Circle Avatar',
+  size: '50px',
+  variant: 'circle',
 };
 
-export const Basic = Template.bind({});
-Basic.parameters = {
-  layout: 'centered',
+export const Square = Template.bind({});
+Square.args = {
+  src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+  alt: 'Square Avatar',
+  size: '50px',
+  variant: 'square',
 };
 
-// Basic.args = {
-//   content: '51+',
-// };
+export const RoundedSquare = Template.bind({});
+RoundedSquare.args = {
+  src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+  alt: 'Rounded Square Avatar',
+  size: '50px',
+  variant: 'rounded-square',
+};
+
+export const CustomSize = Template.bind({});
+CustomSize.args = {
+  src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+  alt: 'Custom Size Avatar',
+  size: '100px',
+  variant: 'circle',
+};
